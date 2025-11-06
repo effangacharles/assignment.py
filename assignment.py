c131faa3bf2e055ec9a84ba624a4a8b42be8ae26
effangacharles <effangacharles447@gmail.com>
2025-11-06 15:03:47 +0100

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,13 @@
+student_record ={
+    "NAME": "charles ededet effanga",
+    "DEPARTMENT": "software engineering",
+    "LEVEL": "300l",
+    "SESSION": "2023/2024",
+    "FACULTY": "computing"
+}
+print(student_record)
+
+student_name = ("charles","bassey","samuel","gideon","emmanuel")
+print(student_name)
+print(student_name[0])
+print(student_name[4])
